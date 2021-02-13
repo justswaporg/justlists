78333cd5338b150f219b1284997db8a330c68dfe
tronconnect2021 <78976327+tronconnect2021@users.noreply.github.com>
2021-02-13 10:43:24 +0530

    Update and rename example.justlists.ts to tronconnect.justlists.ts

---
+++ tronconnect.justlists.ts
@@ -0,0 +1,20 @@
+{
+    "name": "TRONCONNECT",
+    "logoURI": "https://tronconnect.org/images/icon.png",
+    "timestamp": 1613192922,
+    "tokens": [
+        {
+            "chainId": "1003649",
+            "address": "TCQosYwstnEFFXL5iuchzHmdcwXZbmZZaT",
+            "name": "TRONCONNECT",
+            "symbol": "TCX",
+            "decimals": "6",
+            "logoURI": "https://tronconnect.org/images/icon.png"
+        }
+    ],
+    "version": {
+        "major": "1",
+        "minor": "0",
+        "patch": "0"
+    }
+}
