61cdda39e2712710cdd4cbeebce91cb603729743
baronred2020 <74320849+baronred2020@users.noreply.github.com>
2021-03-03 15:16:28 -0500

    Create tetra.justlists.ts

---
+++ tetra.justlists.ts
@@ -0,0 +1,20 @@
+{
+   "name": "TETRA", 
+   "logoURI": "https://coin.top/production/upload/logo/TRkSDmLPUE1vtJp1ewLk4n5fM1YvsZk1yq.png",
+   "timestamp": 1610339529000, 
+   "tokens": [{
+           "chainId": 1, 
+           "address": "TRkSDmLPUE1vtJp1ewLk4n5fM1YvsZk1yq", 
+           "name": "TETRA", 
+           "symbol": "TTR", 
+           "decimals": 8, 
+           "logoURI": "https://coin.top/production/upload/logo/TRkSDmLPUE1vtJp1ewLk4n5fM1YvsZk1yq.png"
+       }
+       
+   ],
+   "version": { 
+       "major": 1, 
+       "minor": 0, 
+       "patch": 0  
+   }
+}
